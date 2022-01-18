f12436df234432b559192c0566184b04beb7a6b3
Jonathan Cornet <cornetj2@gmail.com>
2022-01-18 13:23:58 -0800

    Updated README

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p className="mb-4 text-2xl font-light">A React App made by <strong><a href='https://github.com/cornetj13'>Jonathan Cornet</a></strong> used to search GitHub profiles and see profile details. This project was made by following <a href='https://www.udemy.com/course/modern-react-front-to-back/'>{' '}React Front To Back</a>{' '} Udemy course by <strong><a href='https://traversymedia.com'>Brad Traversy</a></strong>.</p>
+            <p className="text-lg text-gray-400">Version <span className="text-gray-700">1.0.0</span></p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+    return (
+        <div>
+            <h1 className="text-6xl">Welcome to the home page.</h1>
+            <UserResults />
+        </div>
+    )
+}
+
+export default Home
